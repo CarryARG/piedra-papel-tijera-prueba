e401b0dc9973b6c0b3e3131b5ff735e587d7d2fb
Lucas Daniel Carranza <110673534+CarryARG@users.noreply.github.com>
2022-11-28 10:56:00 -0300

    nuevos cambios definidos sin bugs, por ahora
    falta agregar (o inventar) las funciones y listo, lo entregamos
    luego aplicar esta funcionalidad a una pagina que agarre todo js

--- app.js
+++ app.js
@@ -5,17 +5,20 @@ alert('Inicia el Juego!! Elige un ataque:');
 // segundo, el jugador debe elegir una de las 3 opciones
 let confirmacion=1;
 while (confirmacion!=2) {
-    let player=0;
+    let bandera=false;
+    let player=null;
     do{
-        player = prompt('Escribe 0 para PIEDRA, 1 para PAPEL, 2 para TIJERA');
-    }while ((player!=0) && (player!=1) && (player!=2)) 
-    while((player==0) && (player==1) && (player==2)) {
-        alert('aca estoy3');
-        if(player == 0) {
+    player = prompt('Escribe 1 para PIEDRA, 2 para PAPEL, 3 para TIJERA');
+    if((player>=1) && (player<=3)){
+        bandera=true;
+    } else if(bandera=false);
+    }while (bandera==false);
+    while(bandera==true) {
+        if(player == 1) {
             alert('Elegiste PIEDRA');
-        } else if(player == 1) {
-            alert('Elegiste PAPEL');
         } else if(player == 2) {
+            alert('Elegiste PAPEL');
+        } else if(player == 3) {
             alert('Elegiste TIJERA');
         }
 
@@ -30,18 +33,18 @@ while (confirmacion!=2) {
         }
 
         // cuarto, mostrar el resultado
-        if(enemy == player) {
+        if(enemy == (player-1)) {
             alert('Empate! Otra vez!');
-        } else if(enemy == 0 && player == 1) {
+        } else if(enemy == 0 && player == 2) {
             alert('GANASTEEEE WOOO!');
-        } else if(enemy == 1 && player == 2) {
+        } else if(enemy == 1 && player == 3) {
             alert('GANASTEEEE WOOO!');
-        } else if(enemy == 2 && player == 0) {
+        } else if(enemy == 2 && player == 1) {
             alert('GANASTEEEE WOOO!');
         } else {
             alert('PERDISTE :C');
         }
-        player=3;
+        bandera=false;
     }
     do{
         confirmacion = prompt('¿Volver a Jugar? (1 para continuar o 2 para salir)');
